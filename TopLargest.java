32de2f9cad9b274cf9b5442df32fb505511183f3
matthewthartman <matthewthartman@users.noreply.github.com>
2024-08-27 15:52:04 -0500

    Update TopLargest.java

--- TopLargest.java
+++ TopLargest.java
@@ -1,13 +1,12 @@
+/*
+ * Write a function to determine and return the top N values from a list of integers
+*/
+
 import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class TopLargest {
-
-    /**
-     * Write a function to determine and return the top N values from a list of integers
-     *
-     */
 
     public static void main(String[] args) {
         //create array randomly
