d5f74de19a72ce75bbf84c494107f6ba0e82db18
matthewthartman <matthewthartman@users.noreply.github.com>
2024-08-27 15:47:44 -0500

    Update Combo.java

--- Combo.java
+++ Combo.java
@@ -1,20 +1,19 @@
+/**
+ * Code that solves the "combination" problem.
+ *
+ * Example: There are 12 players on a basketball team, and only 5 players can play at any one time.
+ * How many combinations of 5 players can be made with all 12 team members? The formula for this is:
+ *
+ * x = 5! (factorial of the sub group) 120
+ * y = 12! (factorial of the total group) 479,001,600
+ * z = 7! (factorial of the total - sub) 5,040
+ *
+ * y / x * z = Number of unique combinations = 792
+*/
+
 import java.math.BigInteger;
 
-
 public class Combo {
-
-    /**
-     * Code that solves the "combination" problem.
-     *
-     * Example: There are 12 players on a basketball team, and only 5 players can play at any one time.
-     * How many combinations of 5 players can be made with all 12 team members? The formula for this is:
-     *
-     * x = 5! (factorial of the sub group) 120
-     * y = 12! (factorial of the total group) 479,001,600
-     * z = 7! (factorial of the total - sub) 5,040
-     *
-     * y / x * z = Number of unique combinations = 792
-     */
 
     public static void main(String[] args) {
         calculateCombo(5, 12);
